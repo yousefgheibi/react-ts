a5f2702857a6a533c2c83be5dcba238f84414fb3
Yousef Gheibi <gheybi@optt.ca>
2024-11-20 23:05:35 +0330

    create Person && PersonList components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Yousef',
+    last: 'Gheibi'
+  }
+
+  const nameList = [
+    {
+      first: 'Yousef',
+      last: 'Gheibi'
+    },
+    {
+      first: 'Clark',
+      last: 'Kenk'
+    },
+    {
+      first: 'Princess',
+      last: 'Diana'
+    }
+  ]
+
   return (
     <div className="App">
       <Greet name="Yousef" messageCount={10} isLoggedIn={false}/>
+      <Person name={personName}/>
+      <PersonList names={nameList}/>
     </div>
   );
 }
