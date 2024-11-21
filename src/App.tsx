8a2ff3d4b7ac32158ec151314232b104f4ff6c38
Yousef Gheibi <gheybi@optt.ca>
2024-11-21 15:08:39 +0330

    useContext hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
-import { Button } from './components/Button';
-import { Input } from './components/Input';
-import { Container } from './components/Container';
-import { LoggedIn } from './components/state/LoggedIn';
-import { User } from './components/state/User';
-import { Counter } from './components/state/Counter';
+import { Box } from './components/context/Box';
+import { ThemeContextProvider } from './components/context/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
---
+++ src/components/context/theme-css.ts
@@ -0,0 +1,12 @@
+import { text } from "stream/consumers";
+
+export const theme = {
+    primary: {
+        main: '#3f51b5',
+        text: '#fff',
+    },
+    secondary: {
+        main: '#f50057',
+        text: '#fff',
+    }
+}
