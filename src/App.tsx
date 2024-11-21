6d4a43814ac78f67a89b9000316fb4ee5f148214
Yousef Gheibi <gheybi@optt.ca>
2024-11-21 16:36:45 +0330

    useRef hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Box } from './components/context/Box';
 import { ThemeContextProvider } from './components/context/ThemeContext';
+import { DomRef } from './components/ref/DomRef';
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
         <Box />
+        <DomRef />
       </ThemeContextProvider>
     </div>
   );
