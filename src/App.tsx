b874493dbc9560cc028e590908e7c3a7bb510248
Yousef Gheibi <gheybi@optt.ca>
2024-11-21 12:22:05 +0330

    advanced props

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Greet } from './components/Greet';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
 
 function App() {
-  const personName = {
-    first: 'Yousef',
-    last: 'Gheibi'
-  }
-
-  const nameList = [
-    {
-      first: 'Yousef',
-      last: 'Gheibi'
-    },
-    {
-      first: 'Clark',
-      last: 'Kenk'
-    },
-    {
-      first: 'Princess',
-      last: 'Diana'
-    }
-  ]
-
   return (
     <div className="App">
-      <Greet name="Yousef" messageCount={10} isLoggedIn={false}/>
-      <Person name={personName}/>
-      <PersonList names={nameList}/>
+      <Button handleClick={(event, id)=> console.log('clicked', event)} />
+      <Input value="" handleChange={(event)=> console.log(event)} />
     </div>
   );
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+type InputProps = {
+  value: string,
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>)=> void
+}
+
+export const Input = (props: InputProps) => {
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.value)
+    }
+
+    return <input type="text" value={props.value} onChange={handleInputChange} />
+}
